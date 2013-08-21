69aadfab5be87fbddb1fc1dc0be39888693d688d
Neisklar <Neisklar@users.noreply.github.com>
2013-08-21 13:23:35 +0200

    * small fixes for blockexplorer connection
    * removed unusable other coins in network config

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -7,10 +7,10 @@ from . import data
 from p2pool.util import math, pack
 
 nets = dict(
-    ###Neisklar: IMPORTANT!!!!!!!!!!!!!1111!!11einsefl
+    ###Neisklar: IMPORTANT!!!!!!!!!!!!!1111!!11einself
     ###          The SUBSIDY_FUNC is NOT correctly in terms of keeping the minimum 1 QRK
     ###          Reward for the end of the regular mining period. Means: it will work now
-    ###          and some time in the future. I think a simple max(..., 1) aorund it will fix it
+    ###          and some time in the future. I think a simple max(..., 1) around it will fix it
     ###          Maybe the dust threshold should also be rised somewhat, since we only have 5 decimals...
     quarkcoin=math.Object(
         P2P_PREFIX='fea503dd'.decode('hex'),
@@ -27,10 +27,12 @@ nets = dict(
         BLOCK_PERIOD=30, # s
         SYMBOL='QRK',
         CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Quarkcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Quarkcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.quarkcoin'), 'quarkcoin.conf'),
-        BLOCK_EXPLORER_URL_PREFIX='http://192.168.201.164/block/',
-        ADDRESS_EXPLORER_URL_PREFIX='http://192.168.201.164/address/',
+        BLOCK_EXPLORER_URL_PREFIX='http://176.221.46.81/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://176.221.46.81/address/',
         ### Neisklar: normally 2**24 should be 2**20 BUT the quark enabled minerd is coded so that it only detects hashes below 0x000000xxxxxxx
-        ###           and 2*20 would be 0x00000FFFF, maybe chaing that in the miner  would be a good idea for slower ones... 
+        ###           and 2*20 would be 0x00000FFFF, maybe changing that in the miner  would be a good idea for slower ones... 
+		### Update:   the minerd is (at least in GitHub) updated so that it would also detect targets below 2**24 (0x000000xxxx..), (Quarkcoins starts with 2**20 (0x00000xxxx...))
+		###           maybe for new standalone p2pools it's a good choice at the beginning, but ONLY when new hashing power is gradually added...
         #SANE_TARGET_RANGE=(2**256//2**32//1000 - 1, 2**256//2**24 - 1), 
         SANE_TARGET_RANGE=(2**256//2**32//1000 - 1, 2**256//2**20 - 1), 
         DUMB_SCRYPT_DIFF=1,
@@ -51,203 +53,35 @@ nets = dict(
         BLOCK_PERIOD=30, # s
         SYMBOL='tQRK',
         CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Quarkcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Quarkcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.quarkcoin'), 'quarkcoin.conf'),
-        BLOCK_EXPLORER_URL_PREFIX='https://192.168.201.164/block/',
-        ADDRESS_EXPLORER_URL_PREFIX='https://192.168.201.164/address/',
+        BLOCK_EXPLORER_URL_PREFIX='https://none.yet/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='https://none.yet/address/',
         SANE_TARGET_RANGE=(2**256//2**32//1000 - 1, 2**256//2**24 - 1),
         DUMB_SCRYPT_DIFF=1,
         DUST_THRESHOLD=1e8,
     ),
-	
-    quarkcoin_local=math.Object(
-        P2P_PREFIX='011a39f7'.decode('hex'),
-        P2P_PORT=19333,
-        ADDRESS_VERSION=119,
-        RPC_PORT=19334,
-        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
-            'quarkcoinaddress' in (yield bitcoind.rpc_help()) and
-            (yield bitcoind.rpc_getinfo())['testnet']
-        )),
-        SUBSIDY_FUNC=lambda height: 2048*100000000 >> (height + 1)//60480,
-        BLOCKHASH_FUNC=lambda data: pack.IntType(256).unpack(__import__('quark_hash').getPoWHash(data)),
-        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('quark_hash').getPoWHash(data)),
-        BLOCK_PERIOD=30, # s
-        SYMBOL='tQRK',
-        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Quarkcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Quarkcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/quarkcoin-testnet-box/1/'), 'quarkcoin.conf'),
-        BLOCK_EXPLORER_URL_PREFIX='https://none.yet/',
-        ADDRESS_EXPLORER_URL_PREFIX='https://none.yet/',
-        SANE_TARGET_RANGE=(2**256//2**32//1000 - 1, 2**256//2**24 - 1),
-        DUMB_SCRYPT_DIFF=1,
-        DUST_THRESHOLD=1e8,
-    ),	
+    ### Neisklar: that local one was a local testnet
+    #quarkcoin_local=math.Object(
+        #P2P_PREFIX='011a39f7'.decode('hex'),
+        #P2P_PORT=19333,
+        #ADDRESS_VERSION=119,
+        #RPC_PORT=19334,
+        #RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+        #    'quarkcoinaddress' in (yield bitcoind.rpc_help()) and
+        #    (yield bitcoind.rpc_getinfo())['testnet']
+        #)),
+        #SUBSIDY_FUNC=lambda height: 2048*100000000 >> (height + 1)//60480,
+        #BLOCKHASH_FUNC=lambda data: pack.IntType(256).unpack(__import__('quark_hash').getPoWHash(data)),
+        #POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('quark_hash').getPoWHash(data)),
+        #BLOCK_PERIOD=30, # s
+        #SYMBOL='tQRK',
+        #CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Quarkcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Quarkcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/quarkcoin-testnet-box/1/'), 'quarkcoin.conf'),
+        #BLOCK_EXPLORER_URL_PREFIX='https://none.yet/',
+        #ADDRESS_EXPLORER_URL_PREFIX='https://none.yet/',
+        #SANE_TARGET_RANGE=(2**256//2**32//1000 - 1, 2**256//2**24 - 1),
+        #DUMB_SCRYPT_DIFF=1,
+        #DUST_THRESHOLD=1e8,
+    #),	
 
-
-    ###Neisklar: same here as in the other network file:
-    ###          since were some values hardcoded in the code, and also only changed by me
-    ###          this p2pool version likely will not run correctly with the other coins
-	'''
-    bitcoin=math.Object(
-        P2P_PREFIX='f9beb4d9'.decode('hex'),
-        P2P_PORT=8333,
-        ADDRESS_VERSION=0,
-        RPC_PORT=8332,
-        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
-            'bitcoinaddress' in (yield bitcoind.rpc_help()) and
-            not (yield bitcoind.rpc_getinfo())['testnet']
-        )),
-        SUBSIDY_FUNC=lambda height: 50*100000000 >> (height + 1)//210000,
-        POW_FUNC=data.hash256,
-        BLOCK_PERIOD=600, # s
-        SYMBOL='BTC',
-        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Bitcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Bitcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.bitcoin'), 'bitcoin.conf'),
-        BLOCK_EXPLORER_URL_PREFIX='https://blockchain.info/block/',
-        ADDRESS_EXPLORER_URL_PREFIX='https://blockchain.info/address/',
-        SANE_TARGET_RANGE=(2**256//2**32//1000 - 1, 2**256//2**32 - 1),
-        DUMB_SCRYPT_DIFF=1,
-        DUST_THRESHOLD=0.001e8,
-    ),
-    bitcoin_testnet=math.Object(
-        P2P_PREFIX='0b110907'.decode('hex'),
-        P2P_PORT=18333,
-        ADDRESS_VERSION=111,
-        RPC_PORT=18332,
-        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
-            'bitcoinaddress' in (yield bitcoind.rpc_help()) and
-            (yield bitcoind.rpc_getinfo())['testnet']
-        )),
-        SUBSIDY_FUNC=lambda height: 50*100000000 >> (height + 1)//210000,
-        POW_FUNC=data.hash256,
-        BLOCK_PERIOD=600, # s
-        SYMBOL='tBTC',
-        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Bitcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Bitcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.bitcoin'), 'bitcoin.conf'),
-        BLOCK_EXPLORER_URL_PREFIX='http://blockexplorer.com/testnet/block/',
-        ADDRESS_EXPLORER_URL_PREFIX='http://blockexplorer.com/testnet/address/',
-        SANE_TARGET_RANGE=(2**256//2**32//1000 - 1, 2**256//2**32 - 1),
-        DUMB_SCRYPT_DIFF=1,
-        DUST_THRESHOLD=1e8,
-    ),	
-    namecoin=math.Object(
-        P2P_PREFIX='f9beb4fe'.decode('hex'),
-        P2P_PORT=8334,
-        ADDRESS_VERSION=52,
-        RPC_PORT=8332,
-        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
-            'namecoinaddress' in (yield bitcoind.rpc_help()) and
-            not (yield bitcoind.rpc_getinfo())['testnet']
-        )),
-        SUBSIDY_FUNC=lambda height: 50*100000000 >> (height + 1)//210000,
-        POW_FUNC=data.hash256,
-        BLOCK_PERIOD=600, # s
-        SYMBOL='NMC',
-        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Namecoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Namecoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.namecoin'), 'bitcoin.conf'),
-        BLOCK_EXPLORER_URL_PREFIX='http://explorer.dot-bit.org/b/',
-        ADDRESS_EXPLORER_URL_PREFIX='http://explorer.dot-bit.org/a/',
-        SANE_TARGET_RANGE=(2**256//2**32 - 1, 2**256//2**32 - 1),
-        DUMB_SCRYPT_DIFF=1,
-        DUST_THRESHOLD=0.2e8,
-    ),
-    namecoin_testnet=math.Object(
-        P2P_PREFIX='fabfb5fe'.decode('hex'),
-        P2P_PORT=18334,
-        ADDRESS_VERSION=111,
-        RPC_PORT=8332,
-        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
-            'namecoinaddress' in (yield bitcoind.rpc_help()) and
-            (yield bitcoind.rpc_getinfo())['testnet']
-        )),
-        SUBSIDY_FUNC=lambda height: 50*100000000 >> (height + 1)//210000,
-        POW_FUNC=data.hash256,
-        BLOCK_PERIOD=600, # s
-        SYMBOL='tNMC',
-        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Namecoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Namecoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.namecoin'), 'bitcoin.conf'),
-        BLOCK_EXPLORER_URL_PREFIX='http://testnet.explorer.dot-bit.org/b/',
-        ADDRESS_EXPLORER_URL_PREFIX='http://testnet.explorer.dot-bit.org/a/',
-        SANE_TARGET_RANGE=(2**256//2**32 - 1, 2**256//2**32 - 1),
-        DUMB_SCRYPT_DIFF=1,
-        DUST_THRESHOLD=1e8,
-    ),
-    
-    litecoin=math.Object(
-        P2P_PREFIX='fbc0b6db'.decode('hex'),
-        P2P_PORT=9333,
-        ADDRESS_VERSION=48,
-        RPC_PORT=9332,
-        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
-            'litecoinaddress' in (yield bitcoind.rpc_help()) and
-            not (yield bitcoind.rpc_getinfo())['testnet']
-        )),
-        SUBSIDY_FUNC=lambda height: 50*100000000 >> (height + 1)//840000,
-        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data)),
-        BLOCK_PERIOD=150, # s
-        SYMBOL='LTC',
-        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Litecoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Litecoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.litecoin'), 'litecoin.conf'),
-        BLOCK_EXPLORER_URL_PREFIX='http://explorer.litecoin.net/block/',
-        ADDRESS_EXPLORER_URL_PREFIX='http://explorer.litecoin.net/address/',
-        SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
-        DUMB_SCRYPT_DIFF=2**16,
-        DUST_THRESHOLD=0.03e8,
-    ),
-    litecoin_testnet=math.Object(
-        P2P_PREFIX='fcc1b7dc'.decode('hex'),
-        P2P_PORT=19333,
-        ADDRESS_VERSION=111,
-        RPC_PORT=19332,
-        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
-            'litecoinaddress' in (yield bitcoind.rpc_help()) and
-            (yield bitcoind.rpc_getinfo())['testnet']
-        )),
-        SUBSIDY_FUNC=lambda height: 50*100000000 >> (height + 1)//840000,
-        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data)),
-        BLOCK_PERIOD=150, # s
-        SYMBOL='tLTC',
-        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Litecoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Litecoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.litecoin'), 'litecoin.conf'),
-        BLOCK_EXPLORER_URL_PREFIX='http://nonexistent-litecoin-testnet-explorer/block/',
-        ADDRESS_EXPLORER_URL_PREFIX='http://nonexistent-litecoin-testnet-explorer/address/',
-        SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256 - 1),
-        DUMB_SCRYPT_DIFF=2**16,
-        DUST_THRESHOLD=1e8,
-    ),
-
-    terracoin=math.Object(
-        P2P_PREFIX='42babe56'.decode('hex'),
-        P2P_PORT=13333,
-        ADDRESS_VERSION=0,
-        RPC_PORT=13332,
-        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
-            'terracoinaddress' in (yield bitcoind.rpc_help()) and
-            not (yield bitcoind.rpc_getinfo())['testnet']
-        )),
-        SUBSIDY_FUNC=lambda height: 20*100000000 >> (height + 1)//1050000,
-        POW_FUNC=data.hash256,
-        BLOCK_PERIOD=120, # s
-        SYMBOL='TRC',
-        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Terracoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Terracoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.terracoin'), 'terracoin.conf'),
-        BLOCK_EXPLORER_URL_PREFIX='http://cryptocoinexplorer.com:3750/block/',
-        ADDRESS_EXPLORER_URL_PREFIX='http://cryptocoinexplorer.com:3750/address/',
-        SANE_TARGET_RANGE=(2**256//2**32//1000 - 1, 2**256//2**32 - 1),
-        DUMB_SCRYPT_DIFF=1,
-        DUST_THRESHOLD=1e8,
-    ),
-    terracoin_testnet=math.Object(
-        P2P_PREFIX='41babe56'.decode('hex'),
-        P2P_PORT=23333,
-        ADDRESS_VERSION=111,
-        RPC_PORT=23332,
-        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
-            'terracoinaddress' in (yield bitcoind.rpc_help()) and
-            (yield bitcoind.rpc_getinfo())['testnet']
-        )),
-        SUBSIDY_FUNC=lambda height: 20*100000000 >> (height + 1)//1050000,
-        POW_FUNC=data.hash256,
-        BLOCK_PERIOD=120, # s
-        SYMBOL='tTRC',
-        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Terracoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Terracoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.terracoin'), 'terracoin.conf'),
-        BLOCK_EXPLORER_URL_PREFIX='http://cryptocoinexplorer.com:3750/testnet/block/',
-        ADDRESS_EXPLORER_URL_PREFIX='http://cryptocoinexplorer.com:3750/testnet/address/',
-        SANE_TARGET_RANGE=(2**256//2**32//1000 - 1, 2**256//2**32 - 1),
-        DUMB_SCRYPT_DIFF=1,
-        DUST_THRESHOLD=1e8,
-    ),
-    '''
 )
 for net_name, net in nets.iteritems():
     net.NAME = net_name
